d7c0c4f1b5804eec56f923dc2218092a96231d1d
Brandon Carvalho <brandonsouza27@gmail.com>
2021-03-21 15:07:47 -0300

    finished alll cilinder methods

--- cilindro/main.c
+++ cilindro/main.c
@@ -5,8 +5,20 @@
 int main(void){
     
     Cilinder* cilinder = initialize(15, 6);
+    printf("Cilindro inicializado com tamanho 15 e raio 6\n");
+
     float h = height(cilinder);
-    printf("%f", h);
+    printf("Altura do cilindro: %.2f\n", h);
+
+    float r = radius(cilinder);
+    printf("Raio do cilindro: %.2f\n", r);
+
+    float v = volume(cilinder);
+    printf("Volume do cilindro: %.2f\n", v);
+
+    float sa = surfaceArea(cilinder);
+    printf("Area do cilindro: %.2f\n", sa);
+
 
     return 0;
 }
